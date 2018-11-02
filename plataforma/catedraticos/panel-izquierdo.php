2958ec3e2d0334b882bb345766f97aaf6a9cb89e
MARTINEZ <jmtnz97@gmail.com>
2018-11-02 15:21:12 -0600

    Dividiendo el panel superior de catedraticos

--- plataforma/catedraticos/panel-izquierdo.php
+++ plataforma/catedraticos/panel-izquierdo.php
@@ -1,8 +1,8 @@
 <div class="left-sidebar-pro">
         <nav id="sidebar" class="">
             <div class="sidebar-header">
-                <a href="catedraticos.html"><img class="main-logo" src="img/logo/logo.png" alt="" /></a>
-                <strong><a href="catedraticos.html"><img src="img/logo/logosn.png" alt="" /></a></strong>
+                <a href="index.php"><img class="main-logo" src="img/logo/logo.png" alt="" /></a>
+                <strong><a href="index.php"><img src="img/logo/logosn.png" alt="" /></a></strong>
             </div>
             <div class="left-custom-menu-adp-wrap comment-scrollbar">
                 <nav class="sidebar-nav left-sidebar-menu-pro">
