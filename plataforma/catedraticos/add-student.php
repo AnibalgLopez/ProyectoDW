c5c754f5836b56723c0344c2f3f4c700cf02c688
AnibalgLopez <44389173+AnibalgLopez@users.noreply.github.com>
2018-11-02 23:50:08 -0600

    Update plataforma/catedraticos/add-student.php

--- plataforma/catedraticos/add-student.php
+++ plataforma/catedraticos/add-student.php
@@ -6,7 +6,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>Add Student | Kiaalap - Kiaalap Admin Template</title>
+    <title>Ingreso de Alumnos</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- favicon
