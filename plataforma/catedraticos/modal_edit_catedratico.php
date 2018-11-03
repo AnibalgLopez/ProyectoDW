319496f197e61e2fe85290c4b691ffd1dc40fed5
JhonyHernandez <44485912+JhonyHernandez@users.noreply.github.com>
2018-11-03 17:04:20 -0600

    Add files via upload

---
+++ plataforma/catedraticos/modal_edit_catedratico.php
@@ -0,0 +1,47 @@
+<div id="editProductModal" class="modal fade">
+		<div class="modal-dialog">
+			<div class="modal-content">
+				<form name="edit_product" id="edit_product">
+					<div class="modal-header">						
+						<h4 class="modal-title">Editar Nota</h4>
+						<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+					</div>
+					<div class="modal-body">					
+						<div class="form-group">
+							<label>Id Catedratico</label>
+							<input type="text" name="edit_code"  id="edit_code" class="form-control" required>
+							<input type="hidden" name="edit_id" id="edit_id" >
+						</div>
+						<div class="form-group">
+							<label>Nombre</label>
+							<input type="date" name="edit_name" id="edit_name" class="form-control" required>
+						</div>
+                        <div class="form-group">
+							<label>Apellido</label>
+							<input type="text" name="edit_lastname" id="edit_lastname" class="form-control" required>
+						</div>
+						<div class="form-group">
+							<label>Telefono</label>
+							<input type="text" name="edit_category" id="edit_category" class="form-control" required>
+						</div>
+						<div class="form-group">
+							<label>Correo</label>
+							<input type="text" name="edit_stock" id="edit_stock" class="form-control" required>
+						</div>
+						<div class="form-group">
+							<label>Dirección</label>
+							<input type="text" name="edit_price" id="edit_price" class="form-control" required>
+						</div>
+                        <div class="form-group">
+							<label>Id usuario</label>
+							<input type="text" name="edit_vecindad" id="edit_vecindad" class="form-control" required>
+						</div>						
+					</div>
+					<div class="modal-footer">
+						<input type="button" class="btn btn-default" data-dismiss="modal" value="Cancelar">
+						<input type="submit" class="btn btn-info" value="Guardar datos">
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
