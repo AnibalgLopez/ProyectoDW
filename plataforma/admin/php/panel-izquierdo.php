c500d2a3197f9a539b857cb90fec68e3a1b7a1ca
MARTINEZ <jmtnz97@gmail.com>
2018-11-08 14:24:08 -0600

    Pagina admin actualizada

--- plataforma/admin/php/panel-izquierdo.php
+++ plataforma/admin/php/panel-izquierdo.php
@@ -27,6 +27,9 @@
                         <li>
                             <a class="submenu-angle chart-mini-nb-dp" href="cursos.php" aria-expanded="false"><span class="educate-icon educate-course icon-wrap"></span> <span class="mini-click-non">Cursos</span></a>
                         </li>
+                        <li>
+                            <a class="submenu-angle chart-mini-nb-dp" href="asignaciones.php" aria-expanded="false"><span class="educate-icon educate-form icon-wrap"></span> <span class="mini-click-non">Asignaciones</span></a>
+                        </li>
                     </ul>
                 </nav>
             </div>
