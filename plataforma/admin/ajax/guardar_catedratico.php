2c7a183fb9df70643713206cf78a8522f2a119b2
AnibalgLopez <44389173+AnibalgLopez@users.noreply.github.com>
2018-11-09 12:09:23 -0600

    lo olvide joseph

--- plataforma/admin/ajax/guardar_catedratico.php
+++ plataforma/admin/ajax/guardar_catedratico.php
@@ -1,7 +1,7 @@
 <?php
-	/*if (empty($_POST['name'])){
+	if (empty($_POST['nombre'])){
 		$errors[] = "Ingresa el nombre del catedratico.";
-	} elseif (!empty($_POST['name'])){*/
+	} elseif (!empty($_POST['nombre'])){
 	require_once ("../conexion.php");//Contiene funcion que conecta a la base de datos
     // escaping, additionally removing everything that could be (html/javascript-) code
 	$nombre = mysqli_real_escape_string($mysqli,(strip_tags($_POST["nombre"],ENT_QUOTES)));
@@ -9,22 +9,36 @@
 	$telefono = mysqli_real_escape_string($mysqli,(strip_tags($_POST["telefono"],ENT_QUOTES)));
     $correo = mysqli_real_escape_string($mysqli,(strip_tags($_POST["correo"],ENT_QUOTES)));
     $direccion = mysqli_real_escape_string($mysqli,(strip_tags($_POST["direccion"],ENT_QUOTES)));
-    $id_usuario= mysqli_real_escape_string($mysqli,(strip_tags($_POST["id_usuario"],ENT_QUOTES)));
+    $rad = mt_rand(5000,50000);
 	// REGISTRAR INFORMACION A LA BASE DE DATOS
+
+	$sql2  = "INSERT INTO `tb_usuario`(`NOM_USUARIO`, `PASS_USUARIO`, `TIPO_USUARIO`, `FECHA_USUARIO`,`ESTADO_USUARIO`) VALUES ('$correo', '$rad', '1', CURRENT_TIMESTAMP,'1')";
+
+	$query2 = mysqli_query($mysqli, $sql2);
+
+	$sqluserid = mysqli_query($mysqli, "SELECT `ID_USUARIO` FROM `tb_usuario` WHERE `NOM_USUARIO` = '$correo'");
+    $idusuario = mysqli_fetch_array($sqluserid);
+   	$idusuario = $idusuario['ID_USUARIO'];
     
-    $sql = "INSERT INTO TB_CATEDRATICO(NOM_CATEDRATICO,APE_CATEDRATICO,TEL_CATEDRATICO,EMAIL_CATEDRATICO,DIR_CATEDRATICO,ID_USUARIO)
-    VALUES ('$nombre','$apellido', '$telefono', '$correo','$direccion','$id_usuario')";
     
-    $query = mysqli_query($mysqli,$sql);
+    
+    
     
     // if catedratico has been added successfully
-    if ($query) {
+    if ($query2) {
         $messages[] = "El catedratico ha sido guardada con éxito.";
+
+        	$sql = "INSERT INTO TB_CATEDRATICO(NOM_CATEDRATICO,APE_CATEDRATICO,TEL_CATEDRATICO,EMAIL_CATEDRATICO,DIR_CATEDRATICO,ID_USUARIO)
+    VALUES ('$nombre','$apellido', '$telefono', '$correo','$direccion','$idusuario')";
+
+    $query = mysqli_query($mysqli,$sql);
+
     } else {
         $errors[] = "Lo sentimos, el registro falló. Por favor, regrese y vuelva a intentarlo.";
     }
-		
-	//} 
+} 
+
+
 if (isset($errors)){
 			
 			?>
