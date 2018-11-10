6a11be6dc9b6f17eae6c799e095a3cec0c317660
MARTINEZ <jmtnz97@gmail.com>
2018-11-09 20:47:58 -0600

    Modulo ver notas para alumnos agregado

--- plataforma/alumnos/ajax/listar_productos.php
+++ plataforma/alumnos/ajax/listar_productos.php
@@ -2,27 +2,14 @@
 	
 	/* Connect To Database*/
 	require_once ("../conexion.php");
-
+	session_start();
 	
-$action = (isset($_REQUEST['action'])&& $_REQUEST['action'] !=NULL)?$_REQUEST['action']:'';
-if($action == 'ajax'){
+	$action = (isset($_REQUEST['action'])&& $_REQUEST['action'] !=NULL)?$_REQUEST['action']:'';
+	if($action == 'ajax'){
 	$query = mysqli_real_escape_string($mysqli,(strip_tags($_REQUEST['query'], ENT_QUOTES)));
 
 	$tables="tb_notas";
-	$campos="*";
-    $sWhere="vecindad LIKE '%".$query."%'
-	OR lugar LIKE '%".$query."%'
-	OR last_name LIKE '%".$query."%'
-	OR prod_name LIKE '%".$query."%'
-	OR prod_ctry LIKE '%".$query."%'
-	OR prod_qty LIKE '%".$query."%'
-	OR prod_code LIKE '%".$query."%'
-	OR price LIKE '%".$query."%'
-	OR estado LIKE '%".$query."%'
-	OR fecha LIKE '%".$query."%'
-	";
-    $sWhere.=" order by tblprod.prod_name";
-	
+	$campos="*";	
 	
 	include 'pagination.php'; //include pagination file
 	//pagination variables
@@ -38,10 +25,36 @@ if($action == 'ajax'){
 	//main query to fetch the data
 	$query = mysqli_query($mysqli,"SELECT $campos FROM  $tables LIMIT $offset,$per_page");
     //loop through fetched data
-    
 	
+	//realizamos el match el id usuario con la tabla alumnos para encontrar el id alumno
+	$id_match = $_SESSION['usuario']['ID_USUARIO'];
 
+	$query_match = mysqli_query($mysqli,"SELECT tb_alumnos.ID_ALUMNO FROM tb_usuario
+	INNER JOIN tb_alumnos ON tb_usuario.ID_USUARIO=tb_alumnos.ID_USUARIO
+	WHERE tb_usuario.ID_USUARIO = $id_match");
 
+	$resultado_query = mysqli_fetch_array($query_match);
+	$id_alumno = $resultado_query['ID_ALUMNO'];
+
+	//vamos a buscar las notas del alumno con el id alumno obtenido anteriormente
+
+	$query_notas = mysqli_query($mysqli,"SELECT tb_notas.ID_CURSO, tb_notas.PP_NOTA, tb_notas.SP_NOTA, tb_notas.ACT_NOTA, tb_notas.EF_NOTA,
+	SUM(PP_NOTA + SP_NOTA + ACT_NOTA + EF_NOTA) AS TOTAL FROM tb_alumnos
+	INNER JOIN tb_notas ON tb_alumnos.ID_ALUMNO=tb_notas.ID_ALUMNO
+	WHERE tb_alumnos.ID_ALUMNO = $id_alumno");
+
+	$indice = 0;
+	$nom_curso = array();
+
+	while($row = mysqli_fetch_array($query_notas)){
+
+		$id_curso = $row['ID_CURSO'];
+		$query_curso = mysqli_query($mysqli, "SELECT NOM_CURSO FROM TB_CURSO WHERE ID_CURSO = $id_curso");
+		$resultado_query = mysqli_fetch_array($query_curso);
+
+		$nom_curso[$indice] = $resultado_query['NOM_CURSO'];
+		$indice++;
+	}
 		
 	
 	if ($numrows>0){
@@ -50,45 +63,45 @@ if($action == 'ajax'){
 		<div class="table-responsive">
 			<table class="table table-striped table-hover">
 				<thead>
-					<tr>
-						<th class='text-center'>SEMESTRE</th>
-						<th class='text-center'>FECHA</th>
+					<tr><th>
+						<th class='text-center'>CURSO</th>
                         <th class='text-center'>PRIMER PARCIAL</th>
 						<th class='text-center'>SEGUNDO PARCIAL </th>
 						<th class='text-center'>ACTIVIDADES</th>
-						<th class='text-right'>EXAMEN FINAL</th>
-                        <th class='text-center'>CURSO</th>
-                        <th class='text-center'>ALUMNO</th>
-						<th></th>
+						<th class='text-center'>EXAMEN FINAL</th>
+						<th class='text-center'>TOTAL</th>
+						</th>
 					</tr>
 				</thead>
 				<tbody>	
-						<?php 
-						$finales=0;
-						while($row = mysqli_fetch_array($query)){	
-							$product_id=$row['ID_NOTA'];
-							$prod_code=$row['SEM_NOTA'];
-                            $prod_name=$row['FECHA_NOTA'];
-                            $lastname=$row['PP_NOTA'];
-							$prod_ctry=$row['SP_NOTA'];
-							$prod_qty=$row['ACT_NOTA'];
-                            $price=$row['EF_NOTA'];		
-                            $vecindad=$row['ID_CURSO'];
-                            $lugar=$row['ID_ALUMNO'];
+						<?php
+						
+						$query_notas = mysqli_query($mysqli,"SELECT tb_notas.ID_CURSO, tb_notas.PP_NOTA, tb_notas.SP_NOTA, tb_notas.ACT_NOTA, tb_notas.EF_NOTA,
+						SUM(PP_NOTA + SP_NOTA + ACT_NOTA + EF_NOTA) AS TOTAL FROM tb_alumnos
+						INNER JOIN tb_notas ON tb_alumnos.ID_ALUMNO=tb_notas.ID_ALUMNO
+						WHERE tb_alumnos.ID_ALUMNO = $id_alumno");
+
+						$indice = 0; 
+						$finales = 0;
+						while($row = mysqli_fetch_array($query_notas)){	
+                            $parcial1=$row['PP_NOTA'];
+							$parcial2=$row['SP_NOTA'];
+							$actividades=$row['ACT_NOTA'];
+							$examen=$row['EF_NOTA'];
+							$total=$row['TOTAL'];		
 							$finales++;
 						?>	
-						<tr class="<?php;?>">
-							<td class='text-center'><?php echo $prod_code;?></td>
-							<td class='text-center'><?php echo $prod_name;?></td>
-                            <td class='text-center'><?php echo $lastname;?></td>
-							<td class='text-center'><?php echo $prod_ctry;?></td>
-							<td class='text-center'><?php echo $prod_qty;?></td>
-							<td class='text-right'><?php echo $price;?></td>
-                            <td class='text-center'><?php echo $vecindad;?></td>
-                            <td class='text-center'><?php echo $lugar;?></td>
+						<tr class="<?php echo $text_class;?>">
+							<td class='text-center' >
+									<td class='text-center'><?php echo $nom_curso[$indice];?></td>
+									<td class='text-center'><?php echo $parcial1;?></td>
+									<td class='text-center'><?php echo $parcial2;?></td>
+									<td class='text-center'><?php echo $actividades;?></td>
+									<td class='text-center'><?php echo $examen;?></td>
+									<td class='text-center'><?php echo $total;?></td>
+							</td>
 						</tr>
 						<?php }?>
-						<tr>
 							<td colspan='6'> 
 								<?php 
 									$inicios=$offset+1;
--- plataforma/alumnos/php/panel-izquierdo.php
+++ plataforma/alumnos/php/panel-izquierdo.php
@@ -8,13 +8,6 @@
                 <nav class="sidebar-nav left-sidebar-menu-pro">
                     <ul class="metismenu" id="menu1">
                         <li>
-                            <a class="has-arrow" href="all-courses.html" aria-expanded="false"><span class="educate-icon educate-course icon-wrap"></span> <span class="mini-click-non">Cursos</span></a>
-                            <ul class="submenu-angle" aria-expanded="false">
-                                <li><a title="All Courses" href="all-courses.html"><span class="mini-sub-pro">Todos los Cursos</span></a></li>
-                                <li><a title="Courses Profile" href="course-info.html"><span class="mini-sub-pro">Información de Cursos</span></a></li>
-                            </ul>
-                        </li>
-                        <li>
                             <a class="submenu-angle chart-mini-nb-dp" href="notas.php" aria-expanded="false"><span class="educate-icon educate-data-table icon-wrap"></span> <span class="mini-click-non">Notas</span></a>
                         </li>
                         <li>
