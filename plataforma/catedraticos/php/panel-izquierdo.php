6365f619492702e8fd7fcfda039e6a829161bcd5
MARTINEZ <jmtnz97@gmail.com>
2018-11-09 20:51:47 -0600

    Panel de catedraticos actualizado

--- plataforma/catedraticos/php/panel-izquierdo.php
+++ plataforma/catedraticos/php/panel-izquierdo.php
@@ -8,28 +8,10 @@
                 <nav class="sidebar-nav left-sidebar-menu-pro">
                     <ul class="metismenu" id="menu1">
                         
-                        <li class="active">
-                            <a class="has-arrow" href="all-students.html" aria-expanded="false"><span class="educate-icon educate-student icon-wrap"></span> <span class="mini-click-non">Alumnos</span></a>
-                            <ul class="submenu-angle" aria-expanded="false">
-                                <li><a title="All Students" href="all-students.php"><span class="mini-sub-pro">Listado Alumnos</span></a></li>
-                                <li><a title="Students Profile" href="student-profile.php"><span class="mini-sub-pro">Información Alumnos</span></a></li>
-                            </ul>
-                        </li>
-
-                        <li>
-                            <a class="submenu-angle chart-mini-nb-dp" href="catedraticos.php" aria-expanded="false"><span class="educate-icon educate-data-table icon-wrap"></span> <span class="mini-click-non">Catedraticos</span></a>
-                        </li>
-
-                        <li>
-                            <a class="has-arrow" href="all-courses.html" aria-expanded="false"><span class="educate-icon educate-course icon-wrap"></span> <span class="mini-click-non">Cursos</span></a>
-                            <ul class="submenu-angle" aria-expanded="false">
-                                <li><a title="All Courses" href="all-courses.html"><span class="mini-sub-pro">All Courses</span></a></li>
-                                <li><a title="Courses Profile" href="course-info.html"><span class="mini-sub-pro">Courses Info</span></a></li>
-                            </ul>
-                        </li>
                         <li>
                             <a class="submenu-angle chart-mini-nb-dp" href="notas.php" aria-expanded="false"><span class="educate-icon educate-data-table icon-wrap"></span> <span class="mini-click-non">Notas</span></a>
                         </li>
+                        
                         <li>
                                 <a class="submenu-angle" href="salir.php" aria-expanded="false"><span class="educate-icon educate-student icon-wrap"></span> <span class="mini-click-non">Cerrar sesion</span></a>
                         </li>
